066c69bebdab475b28870fabd51406c2033747a0
Matthias Neid <matthias@aternos.org>
2018-09-20 15:54:59 +0200

    upgraded vagrant to ubuntu 18.04

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 sudo sed -i '1 s/^.*$/mclogs.local/' /etc/hostname
-sudo hostname aternos.dev
+sudo hostname mclogs.local
 
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt-get install screen curl python-software-properties zip unzip htop rsync php php-fpm php-mongodb php-xml git nginx mongodb composer redis-server php-redis -y
+sudo apt-get install php-fpm php-mongodb php-xml php-redis nginx mongodb composer redis-server -y
 
 cp /vagrant/nginx/* /etc/nginx/sites-enabled/
-
 sudo service nginx restart
-sudo service php7.0-fpm restart
-
-#cd /web/mclogs && composer install
+sudo service php7.2-fpm restart
