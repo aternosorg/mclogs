b1cc0b13332326fc1da7883b11fd91abbcc0b7a1
Matthias Neid <matthias@aternos.org>
2020-08-18 11:11:54 +0200

    remove unused suggestions and post filters which are now replaced by codex

--- core/config/filter.php
+++ core/config/filter.php
@@ -12,22 +12,4 @@ $config = [
         '\\Filter\\Pre\\Length',
         '\\Filter\\Pre\\Ip'
     ],
-
-    /**
-     * Filters applied before showing log to user
-     *
-     * The classes should implement \Filter\Post\PostFilterInterface
-     */
-    'post' => [
-        '\\Filter\\Post\\HtmlEntities',
-        '\\Filter\\Post\\IndentMultiline',
-        '\\Filter\\Post\\LogLevel',
-        '\\Filter\\Post\\Colors',
-        '\\Filter\\Post\\HighlightPluginErrors',
-        '\\Filter\\Post\\HighlightFMLErrors',
-        '\\Filter\\Post\\LineBreaks',
-        '\\Filter\\Post\\LineNumbers',
-        '\\Filter\\Post\\AddSuggestions'
-    ]
-
 ];
