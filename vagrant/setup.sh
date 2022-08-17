281198df125e3312265cdaa095bb2e39091817cc
Matthias Neid <matthias@aternos.org>
2022-08-17 12:44:21 +0200

    updated codex-minecraft, updated php requirement and php in vagrant to 8.1

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -9,10 +9,10 @@ sudo apt-get upgrade -y
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get update
 
-sudo apt-get install php8.0-fpm php8.0-mongodb php8.0-xml php8.0-redis php8.0-curl nginx mongodb redis-server -y
+sudo apt-get install php8.1-fpm php8.1-mongodb php8.1-xml php8.1-redis php8.1-curl nginx mongodb redis-server -y
 
 bash /web/mclogs/vagrant/setup-composer.sh
 
 cp /web/mclogs/vagrant/nginx/* /etc/nginx/sites-enabled/
 sudo service nginx restart
-sudo service php8.0-fpm restart
+sudo service php8.1-fpm restart
