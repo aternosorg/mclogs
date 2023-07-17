1ef0aa71aa231a7f70eb10e6c42bcc86471547a2
Julian <julian@aternos.org>
2023-07-17 12:57:11 +0200

    open in new tab, change icon

--- web/frontend/logview.php
+++ web/frontend/logview.php
@@ -119,8 +119,8 @@ if (!$log->exists()) {
                             <i class="fa fa-arrow-circle-down"></i>
                             <?=$lineNumbers . " " . $lineString; ?>
                         </div>
-                        <a class="btn btn-white btn-small btn-no-margin" id="raw" href="<?=$urls['apiBaseUrl'] . "/1/raw/". $id->get()?>">
-                            <i class="fa fa-font"></i>
+                        <a class="btn btn-white btn-small btn-no-margin" id="raw" target="_blank" href="<?=$urls['apiBaseUrl'] . "/1/raw/". $id->get()?>">
+                            <i class="fa fa-arrow-up-right-from-square"></i>
                             Raw
                         </a>
                     </div>
