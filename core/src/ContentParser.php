9bac23bc6141ea967e0f1974eea461ce7e2ccc17
Julian Vennen <julian@aternos.org>
2025-12-10 12:56:40 +0100

    Send accept-encoding header

--- core/src/ContentParser.php
+++ core/src/ContentParser.php
@@ -6,12 +6,23 @@
 class ContentParser
 {
     /**
+     * Get all supported content encodings
+     * @return string[]
+     */
+    public function getSupportedEncodings(): array
+    {
+        return ["deflate", "gzip", "x-gzip"];
+    }
+
+    /**
      * Get the content from the http request
      *
      * @return string|ApiError The content string or an ApiError on failure
      */
     public function getContent(): string|ApiError
     {
+        header("Accept-Encoding: " . implode(", ", $this->getSupportedEncodings()));
+
         $body = file_get_contents('php://input');
         if ($body === false) {
             return new ApiError(500, "Failed to read request body.");
