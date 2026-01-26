b5c96e51a1fba4b434cedc0e16e32f51f29ba220
Tom Neumann <tom@aternos.org>
2026-01-26 14:38:04 +0100

    Refactor frontend page layout and UI styles.

--- web/frontend/logview.php
+++ web/frontend/logview.php
@@ -46,16 +46,18 @@ if (!$log) {
     <head>
         <meta name="robots" content="noindex,nofollow">
         <meta charset="utf-8" />
-        <meta name="theme-color" content="#2d3943" />
+        <meta name="theme-color" content="#0a0a0b" />
 
         <title><?=$title; ?> - mclo.gs</title>
 
         <base href="/" />
 
-        <link rel="stylesheet" href="vendor/fonts/fonts.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="vendor/fontawesome/css/all.min.css" />
         <link rel="stylesheet" href="css/btn.css" />
-        <link rel="stylesheet" href="css/mclogs.css?v=071224" />
+        <link rel="stylesheet" href="css/mclogs.css?v=260126b" />
         <link rel="stylesheet" href="css/log.css?v=071222" />
 
         <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
@@ -83,143 +85,128 @@ if (!$log) {
         </script>
     </head>
     <body class="log-body">
-        <header class="row navigation">
-            <div class="row-inner">
-                <a href="/" class="logo">
-                    <img src="img/logo.png" />
-                </a>
-                <div class="menu">
-                    <a class="menu-item" href="/#info">
-                        <i class="fa fa-info-circle"></i> Info
-                    </a>
-                    <a class="menu-item" href="/#plugin">
-                        <i class="fa fa-database"></i> Plugin
-                    </a>
-                    <a class="menu-item" href="/#mod">
-                        <i class="fa fa-puzzle-piece"></i> Mod
-                    </a>
-                    <a class="menu-item" href="/#api">
-                        <i class="fa fa-code"></i> API
-                    </a>
-                    <a class="menu-social btn btn-black btn-notext btn-large btn-no-margin" href="https://github.com/aternosorg/mclogs" target="_blank">
-                        <i class="fab fa-github"></i>
-                    </a>
-                </div>
-            </div>
-        </header>
-        <div class="row dark log-row">
-            <div class="row-inner<?= $shouldWrapLogLines ? "" : " no-wrap"?>">
-                <?php if($log): ?>
-                <div class="log-info">
-                    <div class="log-title">
-                        <h1><i class="fas fa-file-lines"></i> <?=$codexLog->getTitle(); ?></h1>
-                        <div class="log-id">#<?=$id->get(); ?></div>
-                    </div>
-                    <div class="log-info-actions">
-                        <?php if($errorCount): ?>
-                        <div class="btn btn-red btn-small btn-no-margin" id="error-toggle">
-                            <i class="fa fa-exclamation-circle"></i>
-                            <?=$errorCount . " " . $errorString; ?>
-                        </div>
-                        <?php endif; ?>
-                        <div class="btn btn-blue btn-small btn-no-margin" id="down-button">
-                            <i class="fa fa-arrow-circle-down"></i>
-                            <?=$lineNumbers . " " . $lineString; ?>
-                        </div>
-                        <a class="btn btn-white btn-small btn-no-margin" id="raw" target="_blank" href="<?=$log->getRawURL()->toString(); ?>">
-                            <i class="fa fa-arrow-up-right-from-square"></i>
-                            Raw
-                        </a>
-                    </div>
-                </div>
-                <?php if(count($analysis) > 0): ?>
-                    <div class="analysis">
-                        <div class="analysis-headline"><i class="fa fa-info-circle"></i> Analysis</div>
-                        <?php if(count($information) > 0): ?>
-                            <div class="information-list">
-                                <?php foreach($information as $info): ?>
-                                    <div class="information">
-                                        <div class="information-label">
-                                            <?=$info->getLabel(); ?>:
-                                        </div>
-                                        <div class="information-value">
-                                            <?=$info->getValue(); ?>
-                                        </div>
-                                    </div>
-                                <?php endforeach; ?>
+        
+        <div class="container">
+            <header>
+                <a href="/" class="logo" style="text-decoration: none;"><span class="logo-highlight">mclo</span>.gs</a>
+                <div class="tagline"><span class="title-verb">Share</span> your Minecraft logs.</div>
+            </header>
+            <div class="row dark log-row">
+                <div class="row-inner<?= $shouldWrapLogLines ? "" : " no-wrap"?>">
+                    <?php if($log): ?>
+                        <div class="log-info">
+                            <div class="log-title">
+                                <h1><i class="fas fa-file-lines"></i> <?=$codexLog->getTitle(); ?></h1>
+                                <div class="log-id">#<?=$id->get(); ?></div>
                             </div>
-                        <?php endif; ?>
-                        <?php if(count($problems) > 0): ?>
-                            <div class="problem-list">
-                                <?php foreach($problems as $problem): ?>
-                                    <div class="problem">
-                                        <div class="problem">
-                                            <div class="problem-header">
-                                                <div class="problem-message">
-                                                    <i class="fa fa-exclamation-triangle"></i> <?=htmlspecialchars($problem->getMessage()); ?>
+                            <div class="log-info-actions">
+                                <?php if($errorCount): ?>
+                                    <div class="btn btn-red btn-small btn-no-margin" id="error-toggle">
+                                        <i class="fa fa-exclamation-circle"></i>
+                                        <?=$errorCount . " " . $errorString; ?>
+                                    </div>
+                                <?php endif; ?>
+                                <div class="btn btn-blue btn-small btn-no-margin" id="down-button">
+                                    <i class="fa fa-arrow-circle-down"></i>
+                                    <?=$lineNumbers . " " . $lineString; ?>
+                                </div>
+                                <a class="btn btn-white btn-small btn-no-margin" id="raw" target="_blank" href="<?=$log->getRawURL()->toString(); ?>">
+                                    <i class="fa fa-arrow-up-right-from-square"></i>
+                                    Raw
+                                </a>
+                            </div>
+                        </div>
+                        <?php if(count($analysis) > 0): ?>
+                            <div class="analysis">
+                                <div class="analysis-headline"><i class="fa fa-info-circle"></i> Analysis</div>
+                                <?php if(count($information) > 0): ?>
+                                    <div class="information-list">
+                                        <?php foreach($information as $info): ?>
+                                            <div class="information">
+                                                <div class="information-label">
+                                                    <?=$info->getLabel(); ?>:
                                                 </div>
-                                                <?php $number = $problem->getEntry()[0]->getNumber(); ?>
-                                                <a href="/<?=$id->get() . "#L" . $number; ?>" class="btn btn-blue btn-no-margin btn-small" onclick="updateLineNumber('#L<?=$number; ?>');">
-                                                    <span class="hide-mobile"><i class="fa fa-arrow-right"></i> Line </span>#<?=$number; ?>
-                                                </a>
+                                                <div class="information-value">
+                                                    <?=$info->getValue(); ?>
+                                                </div>
                                             </div>
-                                            <div class="problem-body">
-                                                <div class="problem-solution-headline">
-                                                    Solutions
-                                                </div>
-                                                <div class="problem-solution-list">
-                                                    <?php foreach($problem->getSolutions() as $solution): ?>
-                                                        <div class="problem-solution">
-                                                            <?=preg_replace("/'([^']+)'/", "'<strong>$1</strong>'", htmlspecialchars($solution->getMessage())); ?>
+                                        <?php endforeach; ?>
+                                    </div>
+                                <?php endif; ?>
+                                <?php if(count($problems) > 0): ?>
+                                    <div class="problem-list">
+                                        <?php foreach($problems as $problem): ?>
+                                            <div class="problem">
+                                                <div class="problem">
+                                                    <div class="problem-header">
+                                                        <div class="problem-message">
+                                                            <i class="fa fa-exclamation-triangle"></i> <?=htmlspecialchars($problem->getMessage()); ?>
                                                         </div>
-                                                    <?php endforeach; ?>
+                                                        <?php $number = $problem->getEntry()[0]->getNumber(); ?>
+                                                        <a href="/<?=$id->get() . "#L" . $number; ?>" class="btn btn-blue btn-no-margin btn-small" onclick="updateLineNumber('#L<?=$number; ?>');">
+                                                            <span class="hide-mobile"><i class="fa fa-arrow-right"></i> Line </span>#<?=$number; ?>
+                                                        </a>
+                                                    </div>
+                                                    <div class="problem-body">
+                                                        <div class="problem-solution-headline">
+                                                            Solutions
+                                                        </div>
+                                                        <div class="problem-solution-list">
+                                                            <?php foreach($problem->getSolutions() as $solution): ?>
+                                                                <div class="problem-solution">
+                                                                    <?=preg_replace("/'([^']+)'/", "'<strong>$1</strong>'", htmlspecialchars($solution->getMessage())); ?>
+                                                                </div>
+                                                            <?php endforeach; ?>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
+                                        <?php endforeach; ?>
                                     </div>
-                                <?php endforeach; ?>
+                                <?php endif; ?>
                             </div>
                         <?php endif; ?>
-                    </div>
-                <?php endif; ?>
-                <div class="log">
-                    <?php
-                        $log->renew();
-                        echo $log->getPrinter()->print();
-                    ?>
+                        <div class="log">
+                            <?php
+                            $log->renew();
+                            echo $log->getPrinter()->print();
+                            ?>
+                        </div>
+                        <div class="log-bottom">
+                            <div class="btn btn-blue btn-small btn-notext" id="up-button">
+                                <i class="fa fa-arrow-circle-up"></i>
+                            </div>
+                            <div class="checkbox-container">
+                                <input type="checkbox" id="wrap-checkbox"<?=$shouldWrapLogLines ? " checked" : ""?>/>
+                                <label for="wrap-checkbox">Wrap log lines</label>
+                            </div>
+                        </div>
+                        <div class="log-notice">
+                            This log will be saved for 90 days from their last view.<br />
+                            <a href="mailto:<?=$config->get(ConfigKey::LEGAL_ABUSE); ?>?subject=Report%20mclo.gs/<?=$id->get(); ?>">Report abuse</a>
+                        </div>
+                    <?php else: ?>
+                        <div class="not-found">
+                            <div class="not-found-title">404 - Log not found.</div>
+                            <div class="not-found-text">The log you try to open does not exist (anymore).<br />We automatically delete all logs that weren't opened in the last 90 days.</div>
+                            <div class="not-found-buttons">
+                                <a href="/" class="btn btn-no-margin btn-blue btn-small">
+                                    <i class="fa fa-home"></i> Paste a new log
+                                </a>
+                            </div>
+                        </div>
+                    <?php endif; ?>
                 </div>
-                <div class="log-bottom">
-                    <div class="btn btn-blue btn-small btn-notext" id="up-button">
-                        <i class="fa fa-arrow-circle-up"></i>
-                    </div>
-                    <div class="checkbox-container">
-                        <input type="checkbox" id="wrap-checkbox"<?=$shouldWrapLogLines ? " checked" : ""?>/>
-                        <label for="wrap-checkbox">Wrap log lines</label>
-                    </div>
-                </div>
-                <div class="log-notice">
-                    This log will be saved for 90 days from their last view.<br />
-                    <a href="mailto:<?=$config->get(ConfigKey::LEGAL_ABUSE); ?>?subject=Report%20mclo.gs/<?=$id->get(); ?>">Report abuse</a>
-                </div>
-                <?php else: ?>
-                <div class="not-found">
-                    <div class="not-found-title">404 - Log not found.</div>
-                    <div class="not-found-text">The log you try to open does not exist (anymore).<br />We automatically delete all logs that weren't opened in the last 90 days.</div>
-                    <div class="not-found-buttons">
-                        <a href="/" class="btn btn-no-margin btn-blue btn-small">
-                            <i class="fa fa-home"></i> Paste a new log
-                        </a>
-                    </div>
-                </div>
-                <?php endif; ?>
             </div>
-        </div>
-        <div class="row footer">
-            <div class="row-inner">
-                &copy; 2017-<?=date("Y"); ?> by mclo.gs - a service by <a target="_blank" href="https://aternos.org">Aternos</a> |
-                <a target="_blank" href="<?=$config->get(ConfigKey::LEGAL_IMPRINT); ?>">Imprint</a> |
-                <a target="_blank" href="<?=$config->get(ConfigKey::LEGAL_PRIVACY); ?>">Privacy</a>
-            </div>
+            <footer>
+                <nav class="footer-nav">
+                    <a href="https://github.com/aternosorg/mclogs" target="_blank"><i class="fa-brands fa-github"></i> GitHub</a>
+                    <a href="https://modrinth.com/mod/mclogs" target="_blank"><i class="fa-solid fa-cube"></i> Mod</a>
+                    <a href="https://modrinth.com/plugin/mclogs" target="_blank"><i class="fa-solid fa-plug"></i> Plugin</a>
+                    <a href="/api"><i class="fa-solid fa-code"></i> API</a>
+                </nav>
+                <span class="footer-text">&copy; 2017-<?=date("Y"); ?> by mclo.gs - a service by <a href="https://aternos.org" target="_blank">Aternos</a></span>
+            </footer>
         </div>
         <script src="js/logview.js?v=130221"></script>
     </body>
