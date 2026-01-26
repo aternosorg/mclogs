469bb961a4ecf7b6ff7306257a54a572b2004b39
Tom Neumann <tom@aternos.org>
2026-01-26 14:39:13 +0100

    Include btn.css

--- web/frontend/main.php
+++ web/frontend/main.php
@@ -19,6 +19,7 @@ $config = Config::getInstance();
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="vendor/fontawesome/css/all.min.css" />
+        <link rel="stylesheet" href="css/btn.css" />
         <link rel="stylesheet" href="css/mclogs.css?v=260126b" />
 
         <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
