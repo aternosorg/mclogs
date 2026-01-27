0a356bb2c66d51bd89f1a1289bdb10a0dfb8ca0b
Tom Neumann <tom@aternos.org>
2026-01-27 12:17:03 +0100

    adjust logo

--- web/frontend/parts/header.php
+++ web/frontend/parts/header.php
@@ -5,7 +5,13 @@ use Aternos\Mclogs\Util\URL;
 ?>
 <header>
     <a href="/" class="logo" >
-        <img src="img/logo-icon.svg" alt="" />
+        <svg class="logo-icon" width="41" height="42" viewBox="0 0 41 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="41" height="5" rx="2" fill="currentColor"/>
+            <rect y="9.25" width="33" height="5" rx="2" fill="currentColor"/>
+            <rect y="18.5" width="19" height="5" rx="2" fill="currentColor"/>
+            <rect y="27.75" width="33" height="5" rx="2" fill="currentColor"/>
+            <rect y="37" width="41" height="5" rx="2" fill="currentColor"/>
+        </svg>
         <span class="logo-text"><?= URL::getBase()->getHost(); ?></span>
     </a>
     <div class="tagline">
