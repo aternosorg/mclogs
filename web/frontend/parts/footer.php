2ea6cb6ff88d9fab3590622249e03bc6694562d7
Matthias Neid <matthias@aternos.org>
2026-01-27 13:25:23 +0100

    update footer

--- web/frontend/parts/footer.php
+++ web/frontend/parts/footer.php
@@ -1,9 +1,18 @@
+<?php
+use Aternos\Mclogs\Config\Config;use Aternos\Mclogs\Config\ConfigKey;use Aternos\Mclogs\Util\URL;
+?>
 <footer>
     <nav class="footer-nav">
         <a href="https://github.com/aternosorg/mclogs" target="_blank"><i class="fa-brands fa-github"></i> GitHub</a>
-        <a href="https://modrinth.com/mod/mclogs" target="_blank"><i class="fa-solid fa-cube"></i> Mod</a>
-        <a href="https://modrinth.com/plugin/mclogs" target="_blank"><i class="fa-solid fa-plug"></i> Plugin</a>
-        <a href="/api"><i class="fa-solid fa-code"></i> API</a>
+        <a href="https://modrinth.com/plugin/mclogs" target="_blank"><i class="fa-solid fa-cube"></i> Mod/Plugin</a>
+        <a href="<?=URL::getApi()->toString(); ?>"><i class="fa-solid fa-code"></i> API</a>
     </nav>
-    <span class="footer-text">developed by<a href="https://aternos.org" target="_blank"> Aternos</a></span>
+    <span class="footer-text">developed by<a href="https://aternos.org" target="_blank"> Aternos</a>
+        <?php if ($imprintUrl = Config::getInstance()->get(ConfigKey::LEGAL_IMPRINT)): ?>
+            | <a href="<?=htmlspecialchars($imprintUrl); ?>" class="footer-link" target="_blank">Imprint</a>
+        <?php endif; ?>
+        <?php if ($privacyUrl = Config::getInstance()->get(ConfigKey::LEGAL_PRIVACY)): ?>
+            | <a href="<?=htmlspecialchars($privacyUrl); ?>" class="footer-link" target="_blank">Privacy Policy</a>
+        <?php endif; ?>
+    </span>
 </footer>
