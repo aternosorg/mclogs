6e4d180178a79da5a60114b3619b1d01636d68ff
Matthias Neid <matthias@aternos.org>
2026-01-28 12:58:34 +0100

    link logo to start

--- web/frontend/parts/header.php
+++ web/frontend/parts/header.php
@@ -1,5 +1,5 @@
 <header>
-    <a href="/" class="logo">
+    <a href="<?=\Aternos\Mclogs\Util\URL::getBase()->toString(); ?>" class="logo">
         <svg class="logo-icon" width="41" height="42" viewBox="0 0 41 42" fill="none"
              xmlns="http://www.w3.org/2000/svg">
             <rect width="41" height="5" rx="2" fill="currentColor"/>
